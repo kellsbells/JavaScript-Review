6627187bb193380058404a1b38b5fb4f9649e4cc
kellsbells <kelleeballard@hotmail.com>
2014-07-12 22:26:30 -0600

    Completes excercises

--- arrayReview.js
+++ arrayReview.js
@@ -2,14 +2,18 @@ var threeItems = [1,2,3];
 //create a function named 'last' that returns the last object from 'threeItems'
 //alert the result of your function
 
-  //code here
+var last = function() {
+	return threeItems[2];
+}
 
 
 
 //Fix 'dicedArray' below so that it's an array full of integers from 0-10
 var dicedArray = [0,1,4,5,7,8,10];
 
-  //code here
+dicedArray.splice(6, 0, 9);
+dicedArray.splice(4, 0, 6);
+dicedArray.splice(2, 0, 2, 3);
 
 
 
@@ -17,17 +21,43 @@ var dicedArray = [0,1,4,5,7,8,10];
 //hint x % 2 --> checks for even numbers
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
-  //code here
+  for (var i = 0; i < evenArray.length; i++) {
+        if (evenArray[i] % 2 !== 0) {
+        	evenArray.splice(i, 1);
+        	i--;
+        } else {
+        	console.log(evenArray[i]);
+        }
+  }
 
 
 
-//below you're given a function that will return a random number between 0 and 30, and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
+//below you're given a function that will return a random number between 0 and 30, 
+//and an array full or numbers 'randomArray'. Your job is to write a function that will get a 
+//random number, then loop through the array to see if that random number is in the array. If it is, alert true, 
+//if it's not, alert false
+
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 }
 var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
-  //code here
+
+var thingy = function() {
+  var flag = false;
+  var otherThingy = getRandomArbitrary();
+  for(var i = 0; i < randomArray.length; i++) {
+  	if(otherThingy === randomArray[i]) {
+  		flag = true;
+  	}
+  }
+  alert(flag);
+}
+
+thingy();
+
+
+
 
 
 
@@ -35,7 +65,11 @@ var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 var first = [1,2,3,4,5];
 var second;
 
-  //code here
+second = [1,2,3,4,5];
+second.push(6,7);
 
 alert(first) //[1,2,3,4,5];
 alert(second) //[1,2,3,4,5,6,7];
+
+
+
--- objectReview.js
+++ objectReview.js
@@ -1,18 +1,28 @@
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category. 
 
-  //Your code here
+var favoriteThings = {
+  band: "Violet Helm",
+  food: "Tacos",
+  person: "Marcos",
+  book: "Pride and Predjudice",
+  movie: "Monty Python",
+  holiday: "Christmas"
+}
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Your code here
+favoriteThings.car = "Jeep";
+favoriteThings.brand = "Nestle";
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'. 
 
-  //Your code here
+favoriteThings.food = "Lettuce";
+favoriteThings.book = "50 Shades of Gray";
 
 //Now, alert your favorite person, then alert your favorite book.
 
-  //Your code here
+  alert(favoriteThings.person);
+  alert(favoriteThings.book);
 
 
 
@@ -27,15 +37,25 @@ var user = {
 
 //Above you're given a user object. Loop through the user object checking to make sure that each value is truthy. If it's not truthy, remove it from the object. hint: 'delete'.
 
-  //Your code here
+  for(var key in user) {
+    if(!user[key]) {
+      delete user[key]
+    }
+  }
 
 //Once you get your truthy array, Change the remaining values in the array to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
-  //Your code here
+  //How do I get an array?
+  user.name = "Kellee Martins";
+  user.email = "kelleemartins@gmail.com";
+  user.pwHash = "asdfjkl";
+  user.birthday = "Feb. 1, 1992";
+  user.username = "kellsbells";
+  user.age = 22;
 
 //Now console.log your object and make sure it looks right.
 
-  //Your code here
+console.log (user);
 
 
 
@@ -43,12 +63,24 @@ var user = {
 
 //create an empty object called methodCollection. 
 
-  //Your code here
+ var methodCollection = {};
 
 //Now add two methods (functions that are properties on objects) to your methodCollection object. One called 'alertHello' which alerts 'hello' and another method called logHello which logs 'hello' to the console. 
 
-  //Your code here
+methodCollection.alertHello = function_name;
+
 
 //Now call your alertHello and logHello methods. 
 
   //Your code here
+
+
+
+
+
+
+
+
+
+
+
